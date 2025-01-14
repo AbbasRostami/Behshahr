f196c82e44c1a9d8dfa338fc303648914b9c58b2
AbbasRostami <abbas.cracker@gmail.com>
2025-01-15 03:07:56 +0330

    refactor: migrate index and common components from JSX to TypeScript and clean up code

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,11 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Routes from "../routes";
 import { RouterProvider } from "react-router-dom";
-
-import { Landing } from "../screens/Landing";
 import "./App.css";
 import RoutesApp from "../routes";
 import ProfileProvider from "../context/ProfileProvider";
 import { ProgressBar } from "../components/common/ProgressBar";
 
 function App() {
-
-  // Test
   return (
     <>
       <ProfileProvider>
