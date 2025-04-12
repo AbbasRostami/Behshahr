74d84e1f7374f5466866c383461b1c640beb2de0
AbbasRostami <abbas.cracker@gmail.com>
2025-04-13 01:53:12 +0330

    refactor: update PostCSS and Vite configuration for Tailwind CSS integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
+import svgr from "vite-plugin-svgr";
+import tailwindcss from '@tailwindcss/vite'
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      tailwindcss(),
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        // This will transform your SVG to a React component
+        exportType: "named",
+        namedExport: "ReactComponent",
+      },
+    }),
+  ],
 });
