e428e130c0868b90f9bb83d003f95b70a0c14039
AbbasRostami <abbas.cracker@gmail.com>
2025-04-14 02:22:16 +0330

    .

--- src/screens/layout/PanelLayout.tsx
+++ src/screens/layout/PanelLayout.tsx
@@ -1,23 +1,23 @@
-import { useState } from "react";
-import { Outlet, ScrollRestoration } from "react-router-dom";
-import { HeaderDashbord } from "../../components/common/HeaderDashbord/HeaderDashbord";
-import { MainDashbord } from "../../components/common/MainDashbord/mainDashbord";
+// import { useState } from "react";
+// import { Outlet, ScrollRestoration } from "react-router-dom";
+// import { HeaderDashbord } from "../../components/common/HeaderDashbord/HeaderDashbord";
+// import { MainDashbord } from "../../components/common/MainDashbord/mainDashbord";
 
-const PanelLayout = () => {
-  const [showMenu, setShowMenu] = useState(false);
+// const PanelLayout = () => {
+//   const [showMenu, setShowMenu] = useState(false);
 
-  return (
-    <>
-      <div className="dark:bg-slate-900">
-        <HeaderDashbord setShowMenu={setShowMenu} showMenu={showMenu} />
-        <div className="flex mx-[1rem] rtl gap-11">
-          <MainDashbord setShowMenu={setShowMenu} showMenu={showMenu} />
-          <ScrollRestoration />
-          <Outlet />
-        </div>
-      </div>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <div className="dark:bg-slate-900">
+//         <HeaderDashbord setShowMenu={setShowMenu} showMenu={showMenu} />
+//         <div className="flex mx-[1rem] rtl gap-11">
+//           <MainDashbord setShowMenu={setShowMenu} showMenu={showMenu} />
+//           <ScrollRestoration />
+//           <Outlet />
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 
-export default PanelLayout;
+// export default PanelLayout;
